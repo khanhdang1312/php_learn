2353eeff6d32cca0adc7312f223dbedaa00213ca
khanhDang <thanhthaokd2002@gmail.com>
2021-01-25 15:10:33 +0700

    fixed class name in js

--- front-end/Lunar_new_year_lesson/Jquery_exercise/exercise_1/js/main.js
+++ front-end/Lunar_new_year_lesson/Jquery_exercise/exercise_1/js/main.js
@@ -1,7 +1,10 @@
 var calculator=document.getElementById("calculator")
 calculator.addEventListener("click",function()
-    {
+    { 
         var MaxNum=document.getElementById("number").value;
+        if (isNaN(MaxNum)){
+            aler("Giá trị không phải là số!")
+        }
         for(var i=0;i<MaxNum;i++){
             if(i%2==0){
                 document.getElementById("evenNumber").innerHTML+=i+" ";
@@ -12,4 +15,3 @@ calculator.addEventListener("click",function()
         }
     }
 );
-console.log(calculator);
