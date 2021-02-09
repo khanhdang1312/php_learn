f336d3eccbd0253688cd94be3a1b6ec475da0510
khanhDang <thanhthaokd2002@gmail.com>
2021-02-09 14:46:42 +0700

    push new year jquery exercise (push all)

--- front-end/Lunar_new_year_lesson/Jquery_exercise/exercise_4/js/main.js
+++ front-end/Lunar_new_year_lesson/Jquery_exercise/exercise_4/js/main.js
@@ -1,23 +1,53 @@
-var todolist=[];
 var count=0;
-document.getElementById("inputWorks").addEventListener("keydown",addListItem);
-function addListItem(e){
-    var works=document.getElementById("inputWorks").value;
-    resetWorks=document.getElementById("inputWorks");
-    var listWorks=document.getElementById("toDoList");
-    var checkKey=e.keyCode||e.which;
-    if (checkKey==13){
-        if(works.length<=0||works==""){
-            alert("Works don't empty");
+var listItems=document.getElementsByClassName("list-group-item");
+var inputWork=document.getElementById("inputWorks");
+inputWork.addEventListener("keyup",addList);
+function addList(e){
+    var task=document.getElementById("tasks");
+    var checkKey=e.which||e.codeKey;
+    if(checkKey==13){
+        var list=document.getElementById("toDoList");
+        var works=document.getElementById("inputWorks");
+        if(works.value.length==0||works.value==/[0-9]+/i){
+            alert("Công việc không được để trống hoặc chỉ có số");
+            works.className="border-danger form-control";
+            works.focus();
         }
         else{
-                // listWorks.innerHTML="<li class=\'list-group-item d-flex justify-content-between align-item-center\'>"+
-                // todolist[i]+"<i class=\'far fa-trash-alt\' onclick="+"\'deleted("+todolist[i]+")\'"+"></i></li><br>";
-                // todolist.push(works.value);
-                resetWorks.value=0;
-                count++;
+            works.className="border-light form-control";
+            list.innerHTML+="<li class=\'list-group-item d-flex justify-content-between align-item-center\'"+"id=\'listItems"+count+"\'>"+works.value+
+            "<i class=\'far fa-trash-alt trash\' style=\'cursor=:pointer\'" + "id=\'listItems"+count+"\' onclick=\'deleted(this)\'></i>"+"</li>";
+            works.value="";
+            ++count;
+            console.log();
+            if(count>1){
+                task.innerHTML="<b>"+count+" "+"tasks"+"</b>";
+            }
+            else{
+                task.innerHTML=count+" "+"task";
+            }
         }
     }
 }
-// function
-//listWorks = ul#toDoList.list-group.list-group-flush {compact: false, type: "", title: "", lang: "", translate: true, …}
+function deleted(e){
+    for(var i=0;i<listItems.length;i++){
+        console.log(e.id);
+        if(listItems[i].id==e.id){
+            listItems[i].remove();
+            count--;
+        }
+       
+    }
+    if(count<0){
+        count=0;
+    }
+    var task=document.getElementById("tasks");
+    task.innerHTML=count+" "+"task";
+}
+document.getElementById("btnClear").addEventListener("click",function(){
+    var list=document.getElementById("toDoList");
+    list.innerHTML="";
+    count=0;
+    var task=document.getElementById("tasks");
+    task.innerHTML=count+" "+"task";
+})
